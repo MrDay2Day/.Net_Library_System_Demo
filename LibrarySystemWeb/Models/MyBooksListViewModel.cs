990781a28ce085da2d34213ce881d5aaa68d20cd
Dwight Thorpe <dthorpe@day2dayja.com>
2025-04-02 18:05:57 -0500

    Finish!

--- LibrarySystemWeb/Models/MyBooksListViewModel.cs
+++ LibrarySystemWeb/Models/MyBooksListViewModel.cs
@@ -4,6 +4,9 @@
     {
         public int pageNum { get; set; } = 1;
         public int currentPage { get; set; } = 0;
-        public int maxPages{ get; set; } = 0;
+        public int maxPages { get; set; } = 0;
+        public string errMsg { get; set; } = null!;
+
+        public List<(Book book, Borrow borrow)>? myBooks { get; set; } = new List<(Book book, Borrow borrow)>();
     }
 }
--- LibrarySystemWeb/Views/Shared/Components/MyBooksList/MyBooksListViewComponent.cs
+++ LibrarySystemWeb/Views/Shared/Components/MyBooksList/MyBooksListViewComponent.cs
@@ -1,8 +1,10 @@
-﻿using LibrarySystemWeb.Data;
+﻿using System.Drawing.Printing;
+using LibrarySystemWeb.Data;
 using LibrarySystemWeb.Models;
 using LibrarySystemWeb.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LibrarySystemWeb.Views.Shared.Components.MyBooksList
 {
@@ -31,15 +33,66 @@ namespace LibrarySystemWeb.Views.Shared.Components.MyBooksList
 
             var userIdClaim = HttpContext.User.FindFirst("UserId");
 
-            Console.WriteLine($"Page Number: {pageNum}, UserId: {userIdClaim}");
+            int userId = -1;
+            if (userIdClaim != null)
+            {
+                userId = int.Parse(userIdClaim.Value);
+            }
+            else
+            {
+                MyBooksListViewModel errModel = new MyBooksListViewModel()
+                {
+                    errMsg = "Something went wrong"
+                };
+
+                return View(errModel);
+
+            }
+
+
+
+            int perPage = 30;
+
+            int count = _db.Borrows.Select(x => x.UserId == userId && x.ActualReturnDate != null).Count();
+
+            int maxPages = (int)Math.Ceiling((double)count / 30);
+
+
+            Console.WriteLine($"Page Number: {pageNum}\n" +
+                $"UserId: {userId}\n" +
+                $"Count: {count}\n" +
+                $"maxPages: {maxPages}\n\n");
+
 
 
             MyBooksListViewModel model = new MyBooksListViewModel()
             {
                 pageNum = pageNum,
                 currentPage = pageNum,
-                maxPages = 200
+                maxPages = maxPages,
+                errMsg = null!
             };
+
+            if (pageNum > maxPages || pageNum < 1)
+            {
+                pageNum = 1;
+            }
+
+
+            var borrowedBooks = _db.Borrows
+                .Where(borrow => borrow.UserId == userId && borrow.ActualReturnDate == null)
+                .Include(borrow => borrow.Book)
+                .OrderBy(borrow => borrow.ScheduleReturnDate) // Order by ScheduleReturnDate descending
+                .Skip((pageNum - 1) * perPage) // Apply Skip for pagination
+                .Take(perPage) // Apply Take for pagination
+                .ToList();
+
+            var result = borrowedBooks.Select(borrow => (borrow.Book, borrow)).ToList();
+
+
+            model.myBooks = result;
+
+
 
             return View(model);
         }
